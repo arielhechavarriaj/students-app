7bbf00ccfaafccf45e4662378ed4bb7b045c7d5c
Ariel Hechavarria Jardines <ariel.hechavarria@datys.cu>
2023-07-03 02:45:53 -0400

    maquetando add Student

--- src/app/addStudent/page.tsx
+++ src/app/addStudent/page.tsx
@@ -1,14 +1,24 @@
+import {BsSave} from "react-icons/bs";
+import {BiSave} from "react-icons/bi";
+
 export default function addStudent() {
     return (
+<div className={'container-form'}>
 
-        <form action="">
-            <input type="text" placeholder={'Name'}/>
-            <input type="text" placeholder={'Last Name'}/>
-            <input type="email" placeholder={'Last Name'}/>
-            <input type="number" placeholder={'Age'}/>
-            <input type="number" placeholder={'Grade'}/>
+    <form action="" >
+        <p>Add a new Student!</p>
+        <input type="text" placeholder={'Name'}/>
+        <input type="text" placeholder={'Last Name'}/>
+        <input type="email" placeholder={'Last Name'}/>
+        <input type="number" min={1} placeholder={'Age'}/>
+        <input type="number" min={1 }  max={12} placeholder={'Grade'}/>
+        <button className={'button-save'}> <BiSave size={32}/></button>
 
-        </form>
+    </form>
+
+</div>
+
+
     )
 
 }
