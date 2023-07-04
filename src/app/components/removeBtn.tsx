203388c1b3d82ab209632772b83f4e707a652377
Ariel Hechavarria Jardines <ariel.hechavarria@datys.cu>
2023-07-04 11:10:20 -0400

    deploy

--- src/app/components/removeBtn.tsx
+++ src/app/components/removeBtn.tsx
@@ -1,27 +1,27 @@
 "use client"
-import {HiOutlineTrash, HiOutlineUserRemove, HiUserRemove} from "react-icons/hi";
+import {HiUserRemove} from "react-icons/hi";
 import {useRouter} from "next/navigation";
 
 // @ts-ignore
 export default function RemoveBtn({id}) {
-    const router=useRouter();
-   const removeStudent= async ()=>{
-       const confirmed= confirm('Are you sure?');
-       if(confirmed)
-       {
-       const res=    await fetch(`http://localhost:3000/api/students?id=${id}`,{
-               method:'DELETE',
-           })
-           if (res.ok) {
-               router.refresh();
-           }    }
+    const router = useRouter();
+    const removeStudent = async () => {
+        const confirmed = confirm('Are you sure?');
+        if (confirmed) {
+            const res = await fetch(`http://localhost:3000/api/students?id=${id}`, {
+                method: 'DELETE',
+            })
+            if (res.ok) {
+                router.refresh();
+            }
+        }
 
 
-   }
+    }
 
 
     return (
-        <button onClick={removeStudent} >
+        <button onClick={removeStudent}>
 
             <HiUserRemove size={24}/>
         </button>
